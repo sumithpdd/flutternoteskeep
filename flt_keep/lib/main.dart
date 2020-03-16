922eb069c31fa59306c13356e3735cb41c777ef8
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-03-16 15:37:03 +0100

    working version

--- flt_keep/lib/main.dart
+++ flt_keep/lib/main.dart
@@ -1,111 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart' show FirebaseAuth;
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+import 'models.dart' show CurrentUser;
+import 'screens.dart' show HomeScreen, LoginScreen, NoteEditor, SettingsScreen;
+import 'styles.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(NotesApp());
+
+class NotesApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+  Widget build(BuildContext context) => StreamProvider.value(
+    value: FirebaseAuth.instance.onAuthStateChanged.map((user) => CurrentUser.create(user)),
+    initialData: CurrentUser.initial,
+    child: Consumer<CurrentUser>(
+      builder: (context, user, _) => MaterialApp(
+        title: 'Flutter Keep',
+        theme: Theme.of(context).copyWith(
+          brightness: Brightness.light,
+          primaryColor: Colors.white,
+          accentColor: kAccentColorLight,
+          appBarTheme: AppBarTheme.of(context).copyWith(
+            elevation: 0,
+            brightness: Brightness.light,
+            iconTheme: IconThemeData(
+              color: kIconTintLight,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          scaffoldBackgroundColor: Colors.white,
+          bottomAppBarColor: kBottomAppBarColorLight,
+          primaryTextTheme: Theme.of(context).primaryTextTheme.copyWith(
+            // title
+            headline6: const TextStyle(
+              color: kIconTintLight,
             ),
-          ],
+          ),
         ),
+        home: user.isInitialValue
+          ? Scaffold(body: const SizedBox())
+          : user.data != null ? HomeScreen() : LoginScreen(),
+        routes: {
+          '/settings': (_) => SettingsScreen(),
+        },
+        onGenerateRoute: _generateRoute,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ),
+  );
+
+  /// Handle named route
+  Route _generateRoute(RouteSettings settings) {
+    try {
+      return _doGenerateRoute(settings);
+    } catch (e, s) {
+      debugPrint("failed to generate route for $settings: $e $s");
+      return null;
+    }
   }
+
+  Route _doGenerateRoute(RouteSettings settings) {
+    if (settings.name?.isNotEmpty != true) return null;
+
+    final uri = Uri.parse(settings.name);
+    final path = uri.path ?? '';
+    // final q = uri.queryParameters ?? <String, String>{};
+    switch (path) {
+      case '/note': {
+        final note = (settings.arguments as Map ?? {})['note'];
+        return _buildRoute(settings, (_) => NoteEditor(note: note));
+      }
+      default:
+        return null;
+    }
+  }
+
+  /// Create a [Route].
+  Route _buildRoute(RouteSettings settings, WidgetBuilder builder) =>
+    MaterialPageRoute<void>(
+      settings: settings,
+      builder: builder,
+    );
 }
